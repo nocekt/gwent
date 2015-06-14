e05e84c40213fa88e97b73d85016e1d6635279e7
nocekt <nocekt@gmail.com>
2015-06-15 01:34:32 +0200

    i hate writing messages

--- js/style.js
+++ js/style.js
@@ -1,35 +1,36 @@
+$( document ).ready(function() {
+	resize();
+});
 
-/*$( document ).ready(function() {
+var doit;
+window.onresize = function(){
+	clearTimeout(doit);
+	$('.card').css("display","none");
+	doit = setTimeout(function() {
+		resize();
+		$('.card').css("display","inline-block");
+	}, 100);
+};
+
+function resize() {
 	resizeContainers();
 	$('.container').each(function() {
 		resizeCards($(this));
 	});
-});
-
-$( window ).resize( function(){
-	resizeContainers();
-	$('.container').each(function() {
-		resizeCards($(this));
-	});
-});
-*/
+}
 
 function resizeContainers() {
-	var height = $( window ).height();
-	var width = $( window ).width();
-	var margin = $('.container').css("margin-top");
+	
+	var height = $(window).height()-2;
+	var margin = $('.container').css("margin-bottom");
 	margin = margin.substring(0,margin.length-2);
 	
 	var border = $('.container').css("border-left-width");
 	border = border.substring(0,border.length-2);
 	
-	var h = (height-margin-border-50)/7 - margin-2*border;
-	var w = width - 2*margin - 2*border;
-	
+	var h = (height-margin)/6 - margin-2*border;
 	$('.container').css("height", h+"px"); 
-	$('.container').css("width", w+"px");
 }
-
 
 function resizeCards(container) {
 	var height = container.height();
@@ -38,7 +39,10 @@ function resizeCards(container) {
 	
 	var h = height-2*margin;
 	var w = h*2/3;
-	
-	$('.card').css("height", h+"px"); 
-	$('.card').css("width", w+"px");
+	container.find('.card').each(function(){
+		$(this).css("height", h+"px"); 
+		$(this).css("width", w+"px");
+	});
 }
+
+
