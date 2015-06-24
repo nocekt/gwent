871960b5204e75ca2547759314f8da04a0b9a9a9
nocekt <nocekt@gmail.com>
2015-06-25 01:28:13 +0200

    Added layout.

--- js/card.js
+++ js/card.js
@@ -1,1 +1,13 @@
+$( document ).ready(function() {
+	$('.container').each(function() {
+		addCard($(this));
+	});
+});
 
+function addCard(container) {
+	var card = $('<div class="card"></div>');
+	container.append(card);
+
+	// from style.js
+	resizeCards(container);
+}
--- js/style.js
+++ js/style.js
@@ -1,44 +1,23 @@
-$( document ).ready(function() {
-	resize();
-});
-
 var doit;
 window.onresize = function(){
 	clearTimeout(doit);
-	$('.card').css("display","none");
+	//$('.card').css("display","none");
 	doit = setTimeout(function() {
 		resize();
-		$('.card').css("display","inline-block");
+		//$('.card').css("display","inline-block");
 	}, 100);
 };
 
 function resize() {
-	resizeContainers();
 	$('.container').each(function() {
 		resizeCards($(this));
 	});
 }
 
-function resizeContainers() {
-	var padding = $('.panel').css("padding-top");
-	padding = padding.substring(0,padding.length-2);
-	var height = $(window).height()-2*padding;
-	
-	var border = $('.container').css("border-top-width");
-	border = border.substring(0,border.length-2);
-	var h = (height/6)-2*border;
-	$('.container').css("height", h+"px"); 
-}
-
 function resizeCards(container) {
-	var height = container.height();
-	var margin = $('.card').css("margin-top");
-	margin = margin.substring(0,margin.length-2);
-	
-	var h = height-2*margin;
-	var w = h*2/3;
+	var ratio = 2/3;
+	var w = ratio * (container.height());
 	container.find('.card').each(function(){
-		$(this).css("height", h+"px"); 
 		$(this).css("width", w+"px");
 	});
 }
